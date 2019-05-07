a620fcd17bcede6f45ac5f443b879abe1fd0d63f
1a35e1 <1a35e1@protonmail.com>
2019-05-07 11:06:28 +1000

    fixes tests, build the container async  with constants

--- packages/http-server/test/utils/buildTestHarness.ts
+++ packages/http-server/test/utils/buildTestHarness.ts
@@ -4,9 +4,12 @@ import { TestHarness } from '@enso/framework'
 
 import { App } from '../../src/App'
 import { env } from '../../src/config/env'
-import { container } from '../../src/config/container'
+import { buildContainer } from '../../src/config/container'
 
 export async function buildTestHarness (): Promise<TestHarness> {
+  // build the container
+  const container = await buildContainer()
+
   // build the app
   const app = new App(env)
   await app.build(container)
